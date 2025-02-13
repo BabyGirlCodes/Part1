9fa0166344bcb8985ab35bc0546954c0c55768b4
BabyGirlCodes <d0272896@student.dkit.ie>
2025-02-13 12:55:40 +0000

    Question 1,2,3 answered in the app section.

--- src/main/java/app/ShoppingList.java
+++ src/main/java/app/ShoppingList.java
@@ -1,4 +1,6 @@
 package app;
+import java.util.Scanner;
+import utils.DynamicArrayList;
 
 /**
  *
@@ -6,6 +8,18 @@ package app;
  */
 public class ShoppingList {
     public static void main(String[] args) {
-        
+        DynamicArrayList newList = new DynamicArrayList();
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("How many entries would you like to add to your shopping list? ");
+        int numEntries = keyboard.nextInt();
+        keyboard.nextLine();
+
+        System.out.println("Enter your shopping list entries (one item per line):");
+        for (int i = 0; i < numEntries; i++) {
+            String entry = keyboard.nextLine();
+            newList.add(entry);
+        }
+
     }
 }
